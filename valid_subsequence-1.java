eae871a79943215239ffa1963f392fa8d54e34c3
Rahul kr. <117927544+rahulkumar7214@users.noreply.github.com>
2025-07-17 16:59:12 +0530

    Update valid_subsequence-1.java
    additional sollution

--- valid_subsequence-1.java
+++ valid_subsequence-1.java
@@ -1,6 +1,7 @@
 
-// leet -3201. Find the Maximum Length of Valid Subsequence I
+/* leet -3201. Find the Maximum Length of Valid Subsequence I */
 
+/* ----------------------------------------------------------------------method 1-- > by simple Observation */
 class Solution {
     public int maximumLength(int[] nums) {
         int n = nums.length;
@@ -53,7 +54,6 @@ class Solution {
         }
         int len = 1; // At least one number
         int prevParity = nums[0]%2;
-
         for (int i = 1; i < nums.length; ++i) {
             int currParity = nums[i] % 2;
             if (currParity != prevParity) {
@@ -63,5 +63,55 @@ class Solution {
         }
         return Math.max(Math.max(ev, od), len);
     }
+}
+/* ----------------------------------------------------- 2nd method DP method
+---> it will Give TLE because constarint in this problem is (2 <= nums.length <= 2 * 105) , but it is helpfull for solving this problem part-II
+----------------> before using this approach see LIS(tabulation form- Bottom up) leet->300 
+------------------>it is a variant of LIS problem of the dp
+*/  
+class Solution {
     
+    public int maximumLength(int[] nums) {
+        int n  = nums.length;
+
+        // int[] dp1 = new int[n];  // for rem 1
+        // Arrays.fill(dp1, 1);   // initially sabhi ka subsequence to 1 hi rhega
+        
+        // int[] dp0 = new int[n];// for rem 0
+        // Arrays.fill(dp0, 1);
+
+        // instead of taking two diff dp array we can take a 2d arr this will help in   
+        // to cover various rem not only for '2'
+        // i.e it will help in problem (leet 3202) its second version
+
+        int[][] dp = new int[2][n];
+        for(int[] a: dp){
+            Arrays.fill(a, 1);   // initially sabhi ka subsequence to 1 hi rhega
+        }
+
+        int maxsub = 0;
+
+        for(int i=1; i<n; i++){
+            for(int j =0; j<i; j++){
+
+                int rem = (nums[i] + nums[j])%2;
+
+                // if(rem == 1){
+                //     dp1[i] = Math.max(dp1[i], dp1[j]+1);
+                //     maxsub = Math.max(maxsub, dp1[i]);
+                // }else{
+                //     dp0[i] = Math.max(dp0[i], dp0[j]+1);
+                //     maxsub = Math.max(maxsub, dp0[i]);
+                // }
+                // the above was when we took diff arr for diff remainder(rem)
+
+                //rem = 0 = 0th row , %2 = 0 waale case ka result
+                //rem = 1 = 1st row, %2 = 1 waale case ka result
+
+                dp[rem][i] = Math.max(dp[rem][i], dp[rem][j]+1);
+                maxsub = Math.max(maxsub, dp[rem][i]);
+            }
+        }
+        return maxsub;
+    }
 }
