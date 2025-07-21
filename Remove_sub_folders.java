7372d5410b77b437f31694ada47414de053b51ba
Rahul kr. <117927544+rahulkumar7214@users.noreply.github.com>
2025-07-21 21:29:45 +0530

    Update Remove_sub_folders.java

--- Remove_sub_folders.java
+++ Remove_sub_folders.java
@@ -2,3 +2,26 @@
 Date : 19-07-2025
 */
 
+class Solution {
+    public List<String> removeSubfolders(String[] folder) {
+        int n = folder.length;
+        ArrayList<String> al = new ArrayList<>();
+        Arrays.sort(folder);
+
+        for(int i=0; i<n; i++){
+            String curr = folder[i];
+            if(al.size() == 0){
+                al.add(curr);
+            }else{
+
+                String prev = al.get(al.size()-1);
+                if(curr.startsWith(prev) && prev.length()<curr.length() && curr.charAt(prev.length()) == '/'){ // here the condition is given to verify example 3 dry run on it
+                    continue;
+                }
+                //else
+                al.add(curr);
+            }
+        }
+        return al;
+    }
+}
